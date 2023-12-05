6a3288feaa3715ba200bc21e8bda721074114b61
pmosk <pm@ggroupp.ru>
2023-12-05 19:35:25 +0400

    Add appsettings.json to azure host

---
+++ src/hosting/Azure.Hosting/FunctionHost.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Hosting;
+
+namespace GarageGroup.Infra;
+
+public static class FunctionHost
+{
+    public static IHostBuilder CreateFunctionsWorkerBuilderStandard(
+        bool useHostConfiguration = false,
+        Action<IFunctionsWorkerApplicationBuilder>? configure = null)
+        =>
+        new HostBuilder().InternalConfigureFunctionsWorkerStandard(useHostConfiguration, configure);
+}
